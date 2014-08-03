354e27c732d5a7f9a5f36f10bef12adcdf629578
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2014-08-03 12:58:49 +0900

    hash of eems is modified.

--- dotemacs.d/setups/setup-emms.el
+++ dotemacs.d/setups/setup-emms.el
@@ -14,6 +14,26 @@
 
 (add-to-list 'emms-browser-covers "AlbumArtSmall.jpg" "Folder.jpg")
 
+(defun fg-emms-track-description (track)
+  "いくらかナイスなトラック情報を返す."
+  (let ((artist (emms-track-get track 'info-artist))
+        (year (emms-track-get track 'info-year))
+        (album (emms-track-get track 'info-album))
+        (tracknumber (emms-track-get track 'info-tracknumber))
+        (title (emms-track-get track 'info-title)))
+    (cond
+     ((or artist title)
+      (concat (if (> (length artist) 0) artist "Unknown artist") " - "
+              (if (> (length year) 0) year "XXXX") " - "
+              (if (> (length album) 0) album "Unknown album") " - "
+              (if (> (length tracknumber) 0)
+                  (format "%02d" (string-to-number tracknumber))
+                "XX") " - "
+                (if (> (length title) 0) title "Unknown title")))
+     (t
+      (emms-track-simple-description track)))))
+
+(setq emms-track-description-function 'fg-emms-track-description)
 
 (provide 'setup-emms)
 
