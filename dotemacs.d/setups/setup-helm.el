2541e318757ef9c05830dc4f82034a29a6551165
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2014-09-02 16:04:48 +0900

    helm is modified.

--- dotemacs.d/setups/setup-helm.el
+++ dotemacs.d/setups/setup-helm.el
@@ -85,4 +85,56 @@
 (add-to-list 'helm-source-buffers-list
              '(pattern-transformer helm-buffers-list-pattern-transformer))
 
+;; helm-font-families
+(defun helm-font-families ()
+  "Preconfigured `helm' for font family."
+  (interactive)
+  (flet ((helm-mp-highlight-match () nil))
+    (helm-other-buffer
+     '(helm-c-source-font-families)
+     "*helm font families*")))
+
+(defun helm-font-families-create-buffer ()
+  (with-current-buffer
+      (get-buffer-create "*Fonts*")
+    (loop for family in (sort (delete-duplicates (font-family-list)) 'string<)
+          do (insert
+              (propertize (concat family "\n")
+                          'font-lock-face
+                          (list :family family :height 2.0 :weight 'bold))))
+    (font-lock-mode 1)))
+
+(defvar helm-c-source-font-families
+      '((name . "Fonts")
+        (init lambda ()
+              (unless (helm-candidate-buffer)
+                (save-window-excursion
+                  (helm-font-families-create-buffer))
+                (helm-candidate-buffer
+                 (get-buffer "*Fonts*"))))
+        (candidates-in-buffer)
+        (get-line . buffer-substring)
+        (action
+         ("Copy Name" lambda
+          (candidate)
+          (kill-new candidate))
+         ("Insert Name" lambda
+          (candidate)
+          (with-current-buffer helm-current-buffer
+            (insert candidate))))))
+
+(defvar helm-c-source-objc-headline
+  '((name . "Objective-C Headline")
+    (headline  "^[-+@]\\|^#pragma mark")))
+
+(defun objc-headline ()
+  (interactive)
+  ;; Set to 500 so it is displayed even if all methods are not narrowed down.
+  (let ((helm-candidate-number-limit 500))
+    (helm-other-buffer '(helm-c-source-objc-headline)
+                           "*ObjC Headline*")))
+
+(global-set-key "\C-xp" 'objc-headline)
+
+
 (provide 'setup-helm)
