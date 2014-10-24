876f27fab1670965296e7c06f4124c126a8ee175
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2014-10-24 13:52:35 +0900

    elpa updated.

---
+++ dotemacs.d/elpa/moe-theme-20141022.2156/moe-theme-pkg.el
@@ -0,0 +1,4 @@
+(define-package "moe-theme" "20141022.2156" "A colorful eye-candy theme. Moe, moe, kyun!" 'nil)
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ dotemacs.d/elpa/zlc-20141023.830/zlc-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zlc" "20141023.830" "Provides zsh like completion system to Emacs" (quote nil))
