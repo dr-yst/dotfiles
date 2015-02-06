bd582b6e888a6e90fe5477c20be33cb1ca7fffd7
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2015-02-06 20:04:42 +0900

    elpa updated.

---
+++ dotemacs.d/elpa/parsebib-20150205.1305/parsebib-autoloads.el
@@ -0,0 +1,16 @@
+;;; parsebib-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("parsebib-autoloads_flymake.el" "parsebib-pkg.el"
+;;;;;;  "parsebib.el") (21716 40389 405821 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; parsebib-autoloads.el ends here
