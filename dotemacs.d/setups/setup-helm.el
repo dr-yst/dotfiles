ffa1c4649b25b39d6210f562b3d40cb3135ce9fc
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2015-02-20 14:58:00 +0900

    setup-helm modified.

--- dotemacs.d/setups/setup-helm.el
+++ dotemacs.d/setups/setup-helm.el
@@ -33,7 +33,7 @@
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+;; (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
 (global-set-key (kbd "C-c h") 'helm-mini)
 (global-set-key (kbd "C-M-z") 'helm-resume)
 (define-key helm-command-map (kbd "d") 'helm-descbinds)
