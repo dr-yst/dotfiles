74ac4c819a17836070658852255f94d8da8af932
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2015-02-24 17:59:17 +0900

    elpa updated.

--- dotemacs.d/elpa/hiwin-20140521.2134/hiwin-autoloads.el
+++
@@ -1,33 +0,0 @@
-;;; hiwin-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-
-
-;;;### (autoloads (hiwin-deactivate hiwin-activate) "hiwin" "hiwin.el"
-;;;;;;  (21471 9491 0 0))
-;;; Generated autoloads from hiwin.el
-
-(autoload 'hiwin-activate "hiwin" "\
-
-
-\(fn)" t nil)
-
-(autoload 'hiwin-deactivate "hiwin" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("hiwin-pkg.el") (21471 9491 955911 0))
-
-;;;***
-
-(provide 'hiwin-autoloads)
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; coding: utf-8
-;; End:
-;;; hiwin-autoloads.el ends here
