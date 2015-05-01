8b9eb58ff0f310127c40844e0f076f8c12b75cec
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2015-05-01 18:22:32 +0900

    setup.sh added.

---
+++ setup.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+# last updated:<2015/04/02 15:19:17 from WatanabeYoshito-no-iMac.local by yoshito>
+
+echo 'ln -s dotzshrc ~/.zshrc'
+ln -s dotzshrc ~/.zshrc
+
+echo 'ln -s dotemacs.d ~/.emacs.d'
+ln -s dotemacs.d ~/.emacs.d
+
+echo 'ln -s dotgitconfig ~/.gitconfig'
+ln -s dotgitconfig ~/.gitconfig
+
+echo 'ln -s dotgitignore_global ~/.gitignore_global'
+ln -s dotgitignore_global ~/.gitignore_global
+
+echo 'ln -s dotlatexmkrc ~/.latexmkrc'
+ln -s dotlatexmkrc ~/.latexmkrc
+
+echo 'ln -s dottmux.conf ~/.tmux.conf'
+ln -s dottmux.conf ~/.tmux.conf
+
