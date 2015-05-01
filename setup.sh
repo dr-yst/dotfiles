5a5e4868c49106ed9064bf6766b8009aee5f293e
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2015-05-01 18:29:32 +0900

    Paths in setup.sh modified.

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 # !/bin/bash
-# last updated:<2015/04/02 15:19:17 from WatanabeYoshito-no-iMac.local by yoshito>
+# last updated:<2015/05/01 18:28:55 from WatanabeYoshito-no-iMac.local by yoshito>
 
 echo 'ln -s dotzshrc ~/.zshrc'
-ln -s dotzshrc ~/.zshrc
+ln -s ~/.files/dotzshrc ~/.zshrc
 
 echo 'ln -s dotemacs.d ~/.emacs.d'
-ln -s dotemacs.d ~/.emacs.d
+ln -s ~/.files/dotemacs.d ~/.emacs.d
 
 echo 'ln -s dotgitconfig ~/.gitconfig'
-ln -s dotgitconfig ~/.gitconfig
+ln -s ~/.files/dotgitconfig ~/.gitconfig
 
 echo 'ln -s dotgitignore_global ~/.gitignore_global'
-ln -s dotgitignore_global ~/.gitignore_global
+ln -s ~/.files/dotgitignore_global ~/.gitignore_global
 
 echo 'ln -s dotlatexmkrc ~/.latexmkrc'
-ln -s dotlatexmkrc ~/.latexmkrc
+ln -s ~/.files/dotlatexmkrc ~/.latexmkrc
 
 echo 'ln -s dottmux.conf ~/.tmux.conf'
-ln -s dottmux.conf ~/.tmux.conf
+ln -s ~/.files/dottmux.conf ~/.tmux.conf
 
