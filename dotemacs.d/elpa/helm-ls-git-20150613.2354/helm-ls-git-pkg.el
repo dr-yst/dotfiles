687c51fb1f8c6e1a9505e453a1508c130b333f7e
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2015-06-18 19:51:38 +0900

    elpa updated.

---
+++ dotemacs.d/elpa/helm-ls-git-20150613.2354/helm-ls-git-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm-ls-git" "20150613.2354" "list git files." '((helm "1.5")))
--- dotemacs.d/elpa/swift-mode-20150422.2218/swift-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "swift-mode" "20150422.2218" "Major-mode for Apple's Swift programming language." '((emacs "24.4")) :keywords '("languages" "swift"))
