21869d2a5312d7eac02be8dd678e634ac9fb4111
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2015-09-13 16:57:30 +0900

    elpa updated.

---
+++ dotemacs.d/elpa/ebib-20150910.820/ebib-pkg.el
@@ -0,0 +1,9 @@
+(define-package "ebib" "20150910.820" "a BibTeX database manager"
+  '((dash "2.5.0")
+    (parsebib "1.0")
+    (emacs "24.3"))
+  :keywords
+  '("text" "bibtex"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
