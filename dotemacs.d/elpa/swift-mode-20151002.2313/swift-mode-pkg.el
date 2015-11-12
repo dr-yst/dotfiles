68125c1e885d0055c19cbd0e45a2897c3e2bfc49
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2015-11-12 14:18:57 +0900

    elpa updated.

--- dotemacs.d/elpa/swift-mode-20151002.2313/swift-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "swift-mode" "20151002.2313" "Major-mode for Apple's Swift programming language." '((emacs "24.4")) :keywords '("languages" "swift"))
---
+++ dotemacs.d/elpa/zoom-window-20151031.632/zoom-window-pkg.el
@@ -0,0 +1,1 @@
+(define-package "zoom-window" "20151031.632" "Zoom window like tmux" 'nil :url "https://github.com/syohex/emacs-zoom-window")
