3613060e7d5952919d3c30e023b4ed056f0e03ac
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2016-03-24 19:34:22 +0900

    elpa updated.

--- dotemacs.d/elpa/dummy-h-mode-20140816.1233/dummy-h-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "dummy-h-mode" "20140816.1233" "switch major mode to c-/c++-/objc-mode on .h file" (quote nil))
---
+++ dotemacs.d/elpa/dummy-h-mode-20160209.329/dummy-h-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "dummy-h-mode" "20160209.329" "switch major mode to c-/c++-/objc-mode on .h file" 'nil :keywords '("c-mode" "c++-mode" "objc-mode"))
