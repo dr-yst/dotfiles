428a00af85c88f276c97987a669aba44e654545e
dr-yst <sticktrix-yoshito@hotmail.co.jp>
2016-05-02 14:17:19 +0900

    Add dotemacs.d/elpa/ to .gitignore

--- dotemacs.d/elpa/org-agenda-property-20140626.1416/org-agenda-property-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "org-agenda-property" "20140626.1416" "Display org properties in the agenda buffer." (quote ((emacs "24.2"))))
--- dotemacs.d/elpa/sound-wav-20140303.457/sound-wav-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "sound-wav" "20140303.457" "Play wav file" '((deferred "0.3.1") (cl-lib "0.5")) :url "https://github.com/syohex/emacs-sound-wav")
